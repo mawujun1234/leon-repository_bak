99d7efb15987226254a5f0c63ad4c8fd89875d69
mawujun <mawujun1234@163.com>
2018-12-09 22:32:18 +0800

    添加了帮助类

---
+++ src/main/java/com/mawujun/mvc/R.java
@@ -0,0 +1,82 @@
+package com.mawujun.mvc;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * spring mvc 的返回数据格式
+ * 使用方法:R.ok().putData(result);
+ * @author mawujun 16064988
+ *
+ */
+public class R extends HashMap<String, Object> {
+	private static final long serialVersionUID = 1L;
+	
+	public R() {
+		put("code", 0);
+		put("sucess",true);
+		put("msg", "success");
+	}
+	
+	public static R error() {
+		return error(500, "未知异常，请联系管理员");
+	}
+	
+	public static R error(String msg) {
+		return error(500, msg);
+	}
+	
+	public static R error(int code, String msg) {
+		R r = new R();
+		r.put("code", code); 
+		r.put("msg", msg);
+		r.put("success", false);
+		return r;
+	}
+
+	public static R ok(String msg) {
+		R r = new R();
+		r.put("msg", msg);
+		return r;
+	}
+	
+	public static R ok(Map<String, Object> map) {
+		R r = new R();
+		r.putAll(map);
+		return r;
+	}
+	
+	public static R ok() {
+		return new R();
+	}
+
+	@Override
+	public R put(String key, Object value) {
+		super.put(key, value);
+		return this;
+	}
+	
+	public R putData(Object value) {
+		super.put("data", value);
+		return this;
+	}
+	public R setData(Object value) {
+		return this.putData(value);
+	}
+	
+	public R putCode(Object value) {
+		super.put("code", value);
+		return this;
+	}
+	public R setCode(Object value) {
+		return this.putCode(value);
+	}
+	
+	public R putMsg(Object value) {
+		super.put("msg", value);
+		return this;
+	}
+	public R setMsg(Object value) {
+		return this.putMsg(value);
+	}
+}
